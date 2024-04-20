0f8823981ba538e4c3bb918e46f74ef00517c415
dc78999 <c.dinh@uqconnect.edu.au>
2024-04-21 08:58:53 +1000

    find the problem

--- my_mem_cpy.c
+++ my_mem_cpy.c
@@ -11,16 +11,19 @@ int my_str_len(char* string){
     while (string[len]) {
         len++;
     }
-    return len;
+    
 }
 
 char* my_mem_cpy(char* orig) {
     // Declare char array and allocate memory
-    
+    char* array = (char*)malloc(my_str_len(orig) * sizeof(char*));
 
     // copy each char in orig array to result array, including null terminator
-   
+    for (int i = 0; i < my_str_len(orig); i++){
+        array[i] = orig[i];
+    }
 
+    return array;
 }
 
 int main(int argc, char** argv) {
