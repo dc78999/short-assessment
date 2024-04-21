9273270aada8472d40369e4ec042dd0ba84df8ac
Silvia <silviachin1509@gmail.com>
2024-04-21 09:09:32 +0800

    Adding item

--- my_mem_cpy.c
+++ my_mem_cpy.c
@@ -16,18 +16,24 @@ int my_str_len(char* string){
 
 char* my_mem_cpy(char* orig) {
     // Declare char array and allocate memory
-    
+	int g = my_str_len(orig);
+	char* cpyarray = (char*) malloc(my_str_len(orig)*sizeof(char*));	
 
     // copy each char in orig array to result array, including null terminator
-   
-
+	int len =0;
+	while(orig[len] != '\0'){	
+		cpyarray[len] = orig[len];
+		len ++;
+	
+	}
+	return cpyarray;
 }
 
 int main(int argc, char** argv) {
 
     char* orig = "Hello, World";
     char* result = my_mem_cpy(orig);
-
+    
     printf("%s\n", result);
 
     return 0;
